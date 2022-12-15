2bae06c47a87394af950557fa15739b7da842390
emirhanmtl <emirhanmutlu35@gmail.com>
2022-12-15 19:58:52 +0300

    login, register ve redirect sayfaları eklendi.

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', '61Park');
+define('DB_SERVER', '*');
+define('DB_USERNAME', '*');
+define('DB_PASSWORD', '*');
+define('DB_NAME', '*');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($link == false) {
