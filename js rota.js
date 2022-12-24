ccbf21d056860e35eb18f703cd73f48cc1c18b79
emreatalayy <79639419+emreatalayy@users.noreply.github.com>
2022-12-24 21:15:13 +0300

    Add files via upload

---
+++ js rota.js
@@ -0,0 +1,21 @@
+function calculateRoute() {
+  // Başlangıç ve bitiş konumlarını alma
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+
+  // Yandex Maps API'sini kullanarak en kısa yolu hesaplama
+  ymaps.route([start, end]).then(function (route) {
+    var distance = route.getLength(); // Yolun uzunluğunu alın (km cinsinden)
+    var time = route.getTime(); // Yolculuğun süresini alın (dk cinsinden)
+
+    // Yolculuğun detaylarını gösterin
+    alert("Yolculuğun mesafesi: " + distance + " km\nYolculuğun süresi: " + time + " dakika");
+
+    // Haritada en kısa yolu gösterin
+    var map = new ymaps.Map("map", {
+      center: start,
+      zoom: 7
+    });
+    map.geoObjects.add(route);
+  });
+}
