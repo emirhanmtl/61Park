19c1f2f15d63c15818c2a06754b6245d7e72bb8d
Emirhan Mutlu <61901730+emirhanmtl@users.noreply.github.com>
2022-12-27 13:50:45 +0300

    Update config.php

--- 61park/config.php
+++ 61park/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'xxx');
+define('DB_USERNAME', 'xxx');
+define('DB_PASSWORD', 'xxx');
 define('DB_NAME', '61park');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
